33b71dfebc3d511ab643d7fa4da253f27778f3c4
Kartik <kartikeyyadav1002@gmail.com>
2025-07-29 00:25:18 +0530

    more UI

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,84 +1,64 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSideBar = async (req, res) => {
-
-
-    try {
-
-        const loggedInUserId = req.user._id;
-        const filteresUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-        res.status(200).json(filteresUsers);
-
-    } catch (error) {
-        console.log("Error in  getUsersForSideBar", error.message)
-        res.status(500).json({ message: "Internal Server Error" })
-    }
-
-}
-
-
+  try {
+    const loggedInUserId = req.user._id;
+    const filteresUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteresUsers);
+  } catch (error) {
+    console.log("Error in  getUsersForSideBar", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export const getMessages = async (req, res) => {
-    try {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
 
-        const { id: userToChatId } = req.params
-        const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
 
-
-        const messages = await Message.find({
-            $or: [
-                { senderId: myId, receiverId: userToChatId },
-                { senderId: userToChatId, receiverId: myId }
-            ]
-        })
-
-        res.status(200).json(messages);
-
-    } catch (error) {
-        console.log("Error in getMessage", error.message)
-        res.status(500).json({ message: "Internal Server Error" })
-    }
-
-}
-
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export const sendMessages = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const sendMessages = req.user._id;
+    let imageUrl;
 
-
-    try {
-
-        const { text, image } = req.body
-        const { id: receiverId } = req.params;
-        const sendMessages = req.user._id;
-        let imageUrl;
-
-        if (image) {
-            // Upload base64 Image to cloudinary
-            const uploadResponse = await cloudinary.Uploader.Upload(image);
-            imageUrl = uploadResponse.secure_url;
-        }
-
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            text,
-            image: imageUrl
-        })
-
-
-        await newMessage.save();
-
-
-
-
-        res.status(201).json(newMessage);
-
-    } catch (error) {
-        console.log("Error in sendMessage Controller", error.message)
-        res.status(500).json({ message: "Internal Server Error" })
+    if (image) {
+      // Upload base64 Image to cloudinary
+      const uploadResponse = await cloudinary.Uploader.Upload(image);
+      imageUrl = uploadResponse.secure_url;
     }
 
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
 
-}
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage Controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,12 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(
   cors({
@@ -24,6 +26,12 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server UP on port :" + PORT);
